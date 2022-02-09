edac9aa6162f9ab54298d259144d9a632a9f337d
UsagiB4 <binaryBunny@tutanota.com>
2022-02-09 20:02:43 +0600

    Basic GUI

---
+++ 01basic.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+rootWidg = Tk()  # root widget.
+a_lable = Label(rootWidg, text="Hello world")  # creating a label widget
+
+a_lable.pack()  # packing up everything to show on the screen
+rootWidg.mainloop()
